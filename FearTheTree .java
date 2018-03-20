9db08a7ec642acb8fea7ba32c1e04f7fd29d5db5
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-20 21:54:23 +0200

    Create FearTheTree .java

---
+++ FearTheTree .java
@@ -0,0 +1,19 @@
+for(int i = 0; i < 20; i++){
+    print("\\");
+};
+println();
+//middle line
+for(int h = 0; h < 1; h++){
+    for(int g = 0; g< 2; g++){
+        print("|");
+    };
+    print(" FEAR THE TREE! ");
+    for(int d = 0; d< 2; d++){
+        print('|');
+    };
+};
+println();
+for(int s = 0; s < 20; s++){
+    print("/");
+};
+println();
