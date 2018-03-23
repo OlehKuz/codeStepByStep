e594a49861f5059f5d50348131ffa74eb834d331
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-23 12:51:13 +0200

    Update RollTwoDice.java

--- RollTwoDice.java
+++ RollTwoDice.java
@@ -3,6 +3,10 @@ public class RollTwoDice extends ConsoleProgram {
     public void run(){
         int sum = readInt("Desired sum: ");      
         int value = 0;
+        /* In a while loop i need to put println statement after digits and values are computed,
+           so that if i get value == sum, my values still will be printed in the previous run. 
+           If println statement is above those operations, program will check that value == sum, so loop 
+           will not run and values wont get printed*/
         while(value != sum){         
             int digit1 = RandomGenerator.getInstance().nextInt(1, 6);
             int digit2 = RandomGenerator.getInstance().nextInt(1, 6);
