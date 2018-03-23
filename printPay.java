3c407b42acf7bee2f71e893abb7e0be352ebe081
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-23 15:35:20 +0200

    Update printPay.java

--- printPay.java
+++ printPay.java
@@ -1,6 +1,8 @@
 /* If i declare double pay inside an if statement , code will not compile. 
-It sees pay variable only inside that if statement. The best is to declare variable with
-a standart case not inside of any conditions and then assign other values to this variable 
+ If i declare one more double pay after if loop, 
+it will always print the value of this last pay(never w overtime). if i assign pay new values after if condition, 
+where double pay was declared, code will not compile because program sees pay variable only inside that if statement. 
+The best is to declare variable with a standart case not inside of any conditions and then assign other values to this variable 
 in case of my if statement.
 */
 private void printPay(double wage, int hours) {
