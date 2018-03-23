d39fb786685ab35bae3621a524a92b496882a7c1
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-23 15:29:59 +0200

    Update printPay.java

--- printPay.java
+++ printPay.java
@@ -1,3 +1,8 @@
+/* If i declare double pay inside an if statement , code will not compile. 
+It sees pay variable only inside that if statement. The best is to declare variable with
+a standart case not inside of any conditions and then assign other values to this variable 
+in case of my if statement.
+*/
 private void printPay(double wage, int hours) {
     printf("Hours worked: %d\n", hours);
     double pay = hours * wage; 
