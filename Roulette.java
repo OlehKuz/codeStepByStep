9fec12b97bb835a2ef8733ad9d133f85775feb3b
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-28 12:48:36 +0300

    Update Roulette.java

--- Roulette.java
+++ Roulette.java
@@ -1,3 +1,6 @@
+/* Roulette that simulates the gambling game.The player begins with $10 and bets (up to) $3 per spin of the wheel. 
+If the wheel comes up 1-18, the player wins $3. Otherwise, player loses $3.
+Play until the player gets $1000 or drops to $0. At the end, print the max money the player ever earned.*/
 public class Roulette extends ConsoleProgram {
     public void run() {
         while(money > 0 && money < 1000){
@@ -6,6 +9,8 @@ public class Roulette extends ConsoleProgram {
         println("max = $" + count);   
     }
     
+    /* Generates a random number and decides how much money we have left, based on the bet we placed .
+    Calls maxVal method to check the max amount of money we have during the game*/ 
     
     private void spin(){
         int bet = bet();
@@ -19,7 +24,7 @@ public class Roulette extends ConsoleProgram {
         printf("bet $%d, spin %d, money = $%d\n", bet, random, money);
         maxVal();        
     }
-    
+    // was our gamble a win or lose
     private boolean isWin (int random){
         if (random <= 18){
             return true;
@@ -28,13 +33,13 @@ public class Roulette extends ConsoleProgram {
             return false;
         }
     }
-    
+    // counts max amount of money we had during all of our gambles in the game
     private void maxVal() {
         if (money > count){
             count = money;
         }
     }
-    
+    // while we have enough money to bet 3$ - bet 3$, otherwise bet what we have
     private int bet(){
         int bet = 3;
         if (bet > money){
@@ -47,7 +52,7 @@ public class Roulette extends ConsoleProgram {
         int rdm = RandomGenerator.getInstance().nextInt(1, 36);
         return rdm;
     }
-       
+    // private variable that are accessed and modified by each method that calls them   
     private int money = 10;
     private int count = 10;
 }
