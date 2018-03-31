9b86f2cf14d8c2f1a7b06ed43021e3961e8a3ef3
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-31 18:10:28 +0300

    Update playRoulette.java

--- playRoulette.java
+++ playRoulette.java
@@ -5,9 +5,14 @@ Play until the player gets $1000 or drops to $0. At the end, print the max money
     public void playRoulette(int start, int bet) {
         printf("start with $%d, betting up to $%d\n", start, bet);
         printf("bet\tspin\tmoney\n");
+        
+        /* since the value of bet var changes to money left , if we have less money left than amount of initial bet, 
+        we need additional initialBet var that will set bet value back to its starting value when money left 
+        will be enough to make initial bet
+        */
         int initialBet = bet;
         int count = start;
-        while(start > 0){          
+        while (start > 0){          
             if (initialBet > start){
                 bet = start;
             }
