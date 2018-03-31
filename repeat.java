6225e0262d10d2388bc3503d1f7f995fa38bf7eb
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-03-31 19:18:29 +0300

    Update repeat.java

--- repeat.java
+++ repeat.java
@@ -1,11 +1,7 @@
-/*You cant do this in java. I cant just add a string, i need to go through all
-of its characters and add them one by one*/
-
-
 private String repeat(String s, int repetitions){
-    String new = "";
+    String full = "";
     for(int i = 0; i < repetitions; i++){
-        new += s;
+        full += s;
     }
-    return s;
+    return full;
 }
