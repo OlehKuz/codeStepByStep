df5b400f8255af5f39294d895bc14eda2c8255a7
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-09-10 22:35:41 +0300

    Update convertToAltCaps.java

--- convertToAltCaps.java
+++ convertToAltCaps.java
@@ -4,8 +4,18 @@ starting with the first letter in lowercase. For example, the call of convertToA
 
 private String convertToAltCaps (String s) {
     String alt = "";
+    /* convert my string to a lover case, because my new string has to begin w lover case */ 
     String st = s.toLowerCase();
     int len = st.length();
+    
+    /* Idea: set up a counter that will count letters. Letters should alternate between lower and upper case, so they can be 
+    divided into two groups that correspond to even and odd numbers. Even numbers should be lowercase, because first letter which corresponds
+    to index of 0 must be lowercase. If letter is odd, change it to uppercase.
+    
+    Important note: I started up 2 groups on base of if loop index is divisible by 2, without using a counter var. But this way spaces _ or 
+    characters , + = are also counted , and my division of letters stops to function when the program meets the first char other than letter.
+    That is why i need a separate counter var and this condition if (ch >= 'a' && ch <= 'z') {.....}    */
+    
     int counter = 0;
     for (int i = 0; i < len; i ++) {
         char ch = st.charAt(i);
