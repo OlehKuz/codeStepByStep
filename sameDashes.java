09809f4b02ff7ae7ea194cb9223b27b98d3bdf14
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-09-11 12:05:12 +0300

    Update sameDashes.java

--- sameDashes.java
+++ sameDashes.java
@@ -1,8 +1,23 @@
+/*  Write a method named sameDashes that accepts two strings as parameters and that returns a boolean
+    value indicating whether or not they have dashes in the same places.
+    To be considered a match, the strings must have exactly the same number of dashes in exactly the 
+    same positions. Note that the strings might be of different length, but different-length strings 
+    might still return true if it turns out that all of their dashes are in the range of indexes that 
+    is within the bounds of the shorter string.
+    */
+
+
+
 private boolean sameDashes (String s, String st) {
     int lenS = s.length();
     int lenSt = st.length();
     int min = Math.min(lenS,lenSt);
     int max = Math.max(lenS,lenSt);
+    /*  I need to compare strings in the same loop, because if I make two loops, one 
+    for the shorter and one for the longer string, the loops will loop through all values, not only one index,
+    and i will not be able to compare the value of two strings in each index.
+    I need a loop that loops through the length of a smaller string, in order to avoid "index out of bounds error".
+    */
         for (int i = 0; i < min; i++) {
             char c = s.charAt(i);
             char ch = st.charAt(i);
@@ -13,6 +28,7 @@ private boolean sameDashes (String s, String st) {
                 }
             }    
         }
+        /*  Checking if a longer string has any '-' characters on a length, that is longer than the shorter string*/
         if (lenSt > lenS){
             for(int j = min; j < max; j++) {
                 char cha = st.charAt(j);
@@ -28,9 +44,6 @@ private boolean sameDashes (String s, String st) {
                     return false;
                 }
             }
-        }
-        
-       
+        } 
     return true;
-
 }
