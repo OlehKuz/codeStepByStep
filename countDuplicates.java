720bf7ffb40cb6dd9cfbec743085525f691536e0
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-10-18 14:43:14 +0300

    Update countDuplicates.java

--- countDuplicates.java
+++ countDuplicates.java
@@ -5,10 +5,10 @@
     means that there are dublicates , increment dublicate.
     
     [1, 4, 2, 4, 7, 1, 1, 9, 2, 3, 4, 1] counting array[0] dublicates our counter will be 3 the first time,
-    which is what an answer should be, but because we will be looping over array incrementing indexes one in a time, 
+    which is what an answer should be, but because we will be looping over array incrementing indexes one at a time, 
     we will be counting duplicate values a couple of times. we will have 2 dublicates of array[5], but actually those
     will be the same values already counted as array[0] dublicates. of actual k dublicates we will count  !k dublicates.
-    So we need to increment dublicates by 1 if counter is >=1, to avoid the situation of overcounting doubles described before.
+    So we need to increment dublicates by 1 if counter is >=1, to avoid the situation of overcounting dublicates described before.
     */
 
 public int countDuplicates(int[] array){
