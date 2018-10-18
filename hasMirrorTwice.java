8f2b45e2fd76656e7c71c80b72384f530d3020aa
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-10-18 16:46:24 +0300

    Update hasMirrorTwice.java

--- hasMirrorTwice.java
+++ hasMirrorTwice.java
@@ -16,7 +16,9 @@ public boolean hasMirrorTwice(int[] a1, int[] a2){
         /*  loop through a1 in chunks of array 2 size, incrementing by 1 in i loop to shift 
         our "scanner" over the size of a1 array,compare each value of a1 in this chunk to the reversed array a2, 
         if there are as many matches as a2.length, that means that both the chunk and a2, are the same, in that case increment our 
-        repeats var
+        repeats var.   j - i  at its maximum equals to a2.length - 1.  That is why in a check we need to write a2[a2.length - 1 - (j - i)], 
+        and not a2[a2.length - (j - i)], because when j = i = 0, we have a2[a2.length] which gives ArrayOutOfBounds error
+        
         */
         for(int j = i; j < i + a2.length; j++){
             if(a1[j]==a2[a2.length - 1 - (j - i)]){
