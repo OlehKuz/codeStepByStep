ee52c83f00fead363d85dc8dcd2e3c9acae25ba9
OlehKuz <34605524+OlehKuz@users.noreply.github.com>
2018-10-19 16:42:17 +0300

    Update banish.java

--- banish.java
+++ banish.java
@@ -21,12 +21,22 @@ public void banish(int[] a1, int[] a2){
         for(int j = 0; j < a2.length;j++){
             if(a1[i]==a2[j]){
                 counter++;
+                // counter var helps us to keep track at which index the last non-zero element should be 
                 for(int k = i; k < a1.length - counter; k++){
                     a1[k]=a1[k+1];                    
                 }
+                /* i-- ensures that if the next value we shifted in place of previous value in array a1 has the same value
+                as element from a2 array, we will remove it. if we dont have i-- we will not check the current index and any value
+                at that index  will be passed as ligid*/
                 i--;
                 a1[a1.length - counter] = 0;
                 break;
+                /* break statement ensures that after finding the first value that matches a1[i]==a2[j],
+                we will not execute the outermost loop for(int j = 0; j < a2.length;j++){...}   once again.
+                This must be the case, because if we have a match a1[i]==a2[j] when i = 0, then i-- operator will change 
+                i to -1, and checking that condition will throw ArrayIndexOutOFBounds exception. We dont need to check that 
+                condition anyways, because if that number == a2[j], it cant be equal to any other number.
+                */
             }
         }
     }
